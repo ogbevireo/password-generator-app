dddce4215a332dcdb98774f290713e3865e601c1
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-02-05 20:49:45 +0100

    password generator

---
+++ index.js
@@ -0,0 +1,26 @@
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
+
+let passwordOneEl = document.getElementById("password-one")
+let passwordTwoEl = document.getElementById("password-two")
+
+
+function displayed() {
+    reset()
+
+    for (i = 0; i < 15; i++) {
+        let randomIndexOne = characters[Math.floor(Math.random() * characters.length)]
+        let randomIndexTwo = characters[Math.floor(Math.random() * characters.length)]
+        passwordOneEl.value += randomIndexOne
+        passwordTwoEl.value += randomIndexTwo
+    }
+}
+
+function reset() {
+    passwordOneEl.value = ""
+    passwordTwoEl.value = ""
+}
+
+function copyText() {
+
+}
